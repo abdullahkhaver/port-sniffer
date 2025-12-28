cf88d7b862a7371b1dc5116aafae6fe216bcfb8d
Abdullah Khaver <muhammadabdullahkhaver@gmail.com>
2025-12-28 00:37:44 -0500

    readme & imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env; 
+use std::io::{self, Write};
+use std::net::{IpAddr, TcpStream};
+use std::process;
+use std::str::FromStr;
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
+
+const MAX = 65535;
+
+struct Arguments {
+    flag: String,
+    ipaddr : IpAddr,
+    
+}
+
 fn main() {
     println!("Hello, world!");
 }
